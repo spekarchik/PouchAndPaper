f1854edcbffc67aa92ed5c8ba7e8ad3e96d79a56
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-03-28 17:53:03 +0100

    Updated to 1.21.4

--- src/main/java/com/pekar/compactmod/blocks/BlockRegistry.java
+++ src/main/java/com/pekar/compactmod/blocks/BlockRegistry.java
@@ -1,20 +1,67 @@
 package com.pekar.compactmod.blocks;
 
-import com.pekar.compactmod.CompactMod;
-import net.minecraft.world.item.BlockItem;
+import com.pekar.compactmod.Main;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredItem;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static com.pekar.compactmod.Main.BLOCKS;
 
 public class BlockRegistry
 {
-//    public static final DeferredItem<Block> GUNPOWDER_BLOCK = CompactMod.BLOCKS.register("gunpowderblock", GunPowderBlock::new);
-    public static final DeferredBlock<Block> PAPER_BLOCK = CompactMod.BLOCKS.register("paperblock", PaperBlock::new);
-    public static final DeferredItem<BlockItem> PAPER_BLOCK_ITEM = CompactMod.ITEMS.register("paperblock",
-            () -> new ModBlockItem(PAPER_BLOCK.get()));
+    public static final DeferredBlock<Block> PAPER_BLOCK = register("paperblock", PaperBlock::new,
+            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).sound(SoundType.STONE).strength(0.6f));
 
     public static void initStatic()
     {
         // just to initialize static members
+    }
+
+    private static <T extends Block> DeferredBlock<T> register(String name, Supplier<T> supplier)
+    {
+        var blockObject = BLOCKS.register(name, supplier);
+        Main.ITEMS.registerItem(name, p -> new ModBlockItem(blockObject.get(), p));
+        return blockObject;
+    }
+
+    private static <T extends Block> DeferredBlock<T> register(String name, Function<BlockBehaviour.Properties, T> supplier)
+    {
+        var blockObject = BLOCKS.registerBlock(name, supplier);
+        Main.ITEMS.registerItem(name, p -> new ModBlockItem(blockObject.get(), p));
+        return blockObject;
+    }
+
+    private static <T extends Block> DeferredBlock<T> register(String name, Function<BlockBehaviour.Properties, T> supplier, BlockBehaviour.Properties properties)
+    {
+        var blockObject = BLOCKS.registerBlock(name, supplier, properties);
+        Main.ITEMS.registerItem(name, p -> new ModBlockItem(blockObject.get(), p));
+        return blockObject;
+    }
+
+    private static <T extends Block> DeferredBlock<T> register(String name, Supplier<T> blockSupplier, Function<Block, ? extends ModBlockItem> blockItemSupplier)
+    {
+        var blockObject = BLOCKS.register(name, blockSupplier);
+        Main.ITEMS.register(name, () -> blockItemSupplier.apply(blockObject.get()));
+        return blockObject;
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerSkipTab(String name, Supplier<T> supplier)
+    {
+        return BLOCKS.register(name, supplier);
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerSkipTab(String name, Function<BlockBehaviour.Properties, T> supplier)
+    {
+        return BLOCKS.registerBlock(name, supplier);
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerSkipTab(String name, Function<BlockBehaviour.Properties, T> supplier, BlockBehaviour.Properties properties)
+    {
+        return BLOCKS.registerBlock(name, supplier, properties);
     }
 }
--- src/main/java/com/pekar/compactmod/items/ItemRegistry.java
+++ src/main/java/com/pekar/compactmod/items/ItemRegistry.java
@@ -1,21 +1,22 @@
 package com.pekar.compactmod.items;
 
-import com.pekar.compactmod.CompactMod;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 
+import static com.pekar.compactmod.Main.ITEMS;
+
 public class ItemRegistry
 {
-    public static final DeferredItem<Item> PAPER_STACK = CompactMod.ITEMS.register("paperstack", ModItem::new);
-    public static final DeferredItem<Item> INK_BOTTLE = CompactMod.ITEMS.register("ink_bottle", ModItem::new);
-    public static final DeferredItem<Item> GLOW_INK_BOTTLE = CompactMod.ITEMS.register("glow_ink_bottle", ModItem::new);
-    public static final DeferredItem<Item> LEATHER_PACK = CompactMod.ITEMS.register("leatherpack", ModItem::new);
-    public static final DeferredItem<Item> SEEDPOCKET_BEET = CompactMod.ITEMS.register("seedpocket_beet", ModItem::new);
-    public static final DeferredItem<Item> SEEDPOCKET_PUMPKIN = CompactMod.ITEMS.register("seedpocket_pumpkin", ModItem::new);
-    public static final DeferredItem<Item> SEEDPOCKET_WATERMELON = CompactMod.ITEMS.register("seedpocket_watermelon", ModItem::new);
-    public static final DeferredItem<Item> SEEDPOCKET_WHEAT = CompactMod.ITEMS.register("seedpocket_wheat", ModItem::new);
-    public static final DeferredItem<Item> SEEDPOCKET_COCOA = CompactMod.ITEMS.register("seedpocket_cocoa", ModItem::new);
-    public static final DeferredItem<Item> FEATHERS_PACK = CompactMod.ITEMS.register("feathers_pack", ModItem::new);
+    public static final DeferredItem<Item> PAPER_STACK = ITEMS.registerItem("paperstack", ModItem::new);
+    public static final DeferredItem<Item> INK_BOTTLE = ITEMS.registerItem("ink_bottle", ModItem::new);
+    public static final DeferredItem<Item> GLOW_INK_BOTTLE = ITEMS.registerItem("glow_ink_bottle", ModItem::new);
+    public static final DeferredItem<Item> LEATHER_PACK = ITEMS.registerItem("leatherpack", ModItem::new);
+    public static final DeferredItem<Item> SEEDPOCKET_BEET = ITEMS.registerItem("seedpocket_beet", ModItem::new);
+    public static final DeferredItem<Item> SEEDPOCKET_PUMPKIN = ITEMS.registerItem("seedpocket_pumpkin", ModItem::new);
+    public static final DeferredItem<Item> SEEDPOCKET_WATERMELON = ITEMS.registerItem("seedpocket_watermelon", ModItem::new);
+    public static final DeferredItem<Item> SEEDPOCKET_WHEAT = ITEMS.registerItem("seedpocket_wheat", ModItem::new);
+    public static final DeferredItem<Item> SEEDPOCKET_COCOA = ITEMS.registerItem("seedpocket_cocoa", ModItem::new);
+    public static final DeferredItem<Item> FEATHERS_PACK = ITEMS.registerItem("feathers_pack", ModItem::new);
 
     public static void initStatic()
     {
