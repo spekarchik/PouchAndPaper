1344c55c68eee74151440bdc369edc8b7a90908a
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-03-29 22:15:55 +0100

    Bee now fly away from Burned Paper Blocks

--- src/main/java/com/pekar/compactmod/blocks/entity/BurningPaperBlockEntity.java
+++ src/main/java/com/pekar/compactmod/blocks/entity/BurningPaperBlockEntity.java
@@ -1,6 +1,10 @@
 package com.pekar.compactmod.blocks.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -8,10 +12,14 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
 
 public class BurningPaperBlockEntity extends BlockEntity implements BlockEntityTicker<BurningPaperBlockEntity>
 {
     private static final int CREEPER_SEEK_RADUIS = 70;
+    private static final int BEE_SEEK_RADUIS = 50;
 
     public BurningPaperBlockEntity(BlockPos pos, BlockState blockState)
     {
@@ -22,6 +30,7 @@ public class BurningPaperBlockEntity extends BlockEntity implements BlockEntityT
     public void tick(Level level, BlockPos pos, BlockState blockState, BurningPaperBlockEntity burningPaperBlockEntity)
     {
         if (level.getGameTime() % 40 != 0) return;
+        if (!(level instanceof ServerLevel serverLevel)) return;
         if (level.getBlockState(pos.above()).is(Blocks.FIRE)) return;
 
         var creepers = level.getEntitiesOfClass(Creeper.class, new AABB(pos).inflate(CREEPER_SEEK_RADUIS));
@@ -30,6 +39,37 @@ public class BurningPaperBlockEntity extends BlockEntity implements BlockEntityT
         {
             var navigation = creeper.getNavigation();
             navigation.moveTo(pos.getX(), pos.getY(), pos.getZ(), 1.0);
+        }
+
+        makeBeesAvoid(serverLevel, pos);
+    }
+
+    private void makeBeesAvoid(ServerLevel level, BlockPos pos)
+    {
+        List<Bee> bees = level.getEntitiesOfClass(Bee.class, new AABB(pos).inflate(BEE_SEEK_RADUIS));
+
+        for (Bee bee : bees)
+        {
+            var navigation = bee.getNavigation();
+            bee.setHivePos(null);
+            bee.setSavedFlowerPos(null);
+
+            Vec3 beePos = bee.position();
+
+            if (navigation.isDone() || navigation.isStuck())
+            {
+                Vec3 blockCenter = Vec3.atCenterOf(pos);
+                Vec3 escapeDirection = beePos.subtract(blockCenter).normalize().scale(BEE_SEEK_RADUIS);
+                Vec3 escapePosVec = beePos.add(escapeDirection);
+
+                int escapeY = escapePosVec.y < blockCenter.y ? pos.getY() + 1 : (int)escapePosVec.y;
+                BlockPos escapePos = new BlockPos((int)escapePosVec.x, escapeY, (int)escapePosVec.z);
+
+                if (level.getBlockState(escapePos).isAir())
+                {
+                    navigation.moveTo(escapePos.getX(), escapePos.getY(), escapePos.getZ(), 1.2);
+                }
+            }
         }
     }
 }
