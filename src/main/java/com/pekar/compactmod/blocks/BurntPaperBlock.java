64b5ab53a92ea54bd88de0fa82b30f4727fc8bcb
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-04-30 02:03:15 +0200

    Burnt Block: 'stepOn' behavior improved (not affected by bees)

--- src/main/java/com/pekar/compactmod/blocks/BurntPaperBlock.java
+++ src/main/java/com/pekar/compactmod/blocks/BurntPaperBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.BlockGetter;
@@ -65,13 +66,14 @@ public class BurntPaperBlock extends PaperBlock implements EntityBlock
     public void fallOn(Level level, BlockState blockState, BlockPos pos, Entity entity, double fallDistance)
     {
         if (entity instanceof Creeper && fallDistance < 0.5f) return;
+        if (entity instanceof Bee) return;
         level.destroyBlock(pos, false);
     }
 
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity)
     {
-        if (entity instanceof Creeper) return;
+        if (entity instanceof Creeper || entity instanceof Bee) return;
         level.destroyBlock(pos, false);
     }
 
