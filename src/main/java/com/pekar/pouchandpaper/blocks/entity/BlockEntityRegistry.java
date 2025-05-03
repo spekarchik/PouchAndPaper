757e7bb072c6235c93e82eca377997eccf80bef8
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-03 02:07:43 +0200

    'compactmod' renamed to 'pouchandpaper'

---
+++ src/main/java/com/pekar/pouchandpaper/blocks/entity/BlockEntityRegistry.java
@@ -0,0 +1,18 @@
+package com.pekar.pouchandpaper.blocks.entity;
+
+import com.pekar.pouchandpaper.Main;
+import com.pekar.pouchandpaper.blocks.BlockRegistry;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.neoforged.neoforge.registries.DeferredHolder;
+
+public class BlockEntityRegistry
+{
+    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<BurntPaperBlockEntity>> BURNT_PAPER_BLOCK_ENTITY =
+            Main.BLOCK_ENTITIES.register("burnt_paper_block_entity", () ->
+                    new BlockEntityType<>(BurntPaperBlockEntity::new, BlockRegistry.BURNT_PAPER_BLOCK.get()));
+
+    public static void initStatic()
+    {
+        // just to initialize static members
+    }
+}
