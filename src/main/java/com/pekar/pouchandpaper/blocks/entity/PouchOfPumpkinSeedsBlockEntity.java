6860ab028e228ecd16525b2e663fcf9c05415f02
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-16 00:51:00 +0200

    Pouches of Watermelon, Pumpkin, Cocoa: the items replaced with blocks

---
+++ src/main/java/com/pekar/pouchandpaper/blocks/entity/PouchOfPumpkinSeedsBlockEntity.java
@@ -0,0 +1,14 @@
+package com.pekar.pouchandpaper.blocks.entity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class PouchOfPumpkinSeedsBlockEntity extends PouchOfSeedsBlockEntity
+{
+
+    public PouchOfPumpkinSeedsBlockEntity(BlockPos pos, BlockState blockState)
+    {
+        super(BlockEntityRegistry.POUCH_OF_PUMPKIN_BLOCK_ENTITY.get(), pos, blockState);
+    }
+
+}
