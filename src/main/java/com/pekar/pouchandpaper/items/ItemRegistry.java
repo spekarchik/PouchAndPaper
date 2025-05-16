ff91d98d950b8b2b974754afbb484c1e6f222564
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-16 18:09:54 +0200

    cleanup
    (cherry picked from commit ec475bcb0f4f29cbef14252787d8b8dc82f2e0c7)

--- src/main/java/com/pekar/pouchandpaper/items/ItemRegistry.java
+++ src/main/java/com/pekar/pouchandpaper/items/ItemRegistry.java
@@ -11,10 +11,6 @@ public class ItemRegistry
     public static final DeferredItem<Item> INK_BOTTLE = ITEMS.registerItem("ink_bottle", ModItem::new);
     public static final DeferredItem<Item> GLOW_INK_BOTTLE = ITEMS.registerItem("glow_ink_bottle", ModItem::new);
     public static final DeferredItem<Item> LEATHER_PACK = ITEMS.registerItem("leatherpack", ModItem::new);
-//    public static final DeferredItem<Item> SEEDPOCKET_BEET = ITEMS.registerItem("seedpocket_beet", ModItem::new);
-//    public static final DeferredItem<Item> SEEDPOCKET_PUMPKIN = ITEMS.registerItem("seedpocket_pumpkin", ModItem::new);
-//    public static final DeferredItem<Item> SEEDPOCKET_WATERMELON = ITEMS.registerItem("seedpocket_watermelon", ModItem::new);
-//    public static final DeferredItem<Item> SEEDPOCKET_COCOA = ITEMS.registerItem("seedpocket_cocoa", ModItem::new);
     public static final DeferredItem<Item> FEATHERS_PACK = ITEMS.registerItem("feathers_pack", ModItem::new);
 
     public static void initStatic()
