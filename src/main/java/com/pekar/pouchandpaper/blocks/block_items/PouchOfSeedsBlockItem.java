c375cc112f83b7c5f6a85e92b9a2e8bfe8d248ce
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-16 21:33:51 +0200

    Pouches as blocks: implemented extended tooltips

--- src/main/java/com/pekar/pouchandpaper/blocks/block_items/PouchOfSeedsBlockItem.java
+++ src/main/java/com/pekar/pouchandpaper/blocks/block_items/PouchOfSeedsBlockItem.java
@@ -1,11 +1,44 @@
 package com.pekar.pouchandpaper.blocks.block_items;
 
+import com.pekar.pouchandpaper.tooltip.ITooltip;
+import com.pekar.pouchandpaper.tooltip.ITooltipProvider;
+import com.pekar.pouchandpaper.utils.Utils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.block.Block;
 
-public class PouchOfSeedsBlockItem extends ModBlockItem
+import java.util.function.Consumer;
+
+public class PouchOfSeedsBlockItem extends ModBlockItem implements ITooltipProvider
 {
+    protected final Utils utils = new Utils();
+
     public PouchOfSeedsBlockItem(Block block, Properties properties)
     {
         super(block, properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay tooltipDisplay, Consumer<Component> componentConsumer, TooltipFlag tooltipFlag)
+    {
+        ITooltipProvider.appendHoverText(this, stack, context, tooltipDisplay, componentConsumer, tooltipFlag);
+    }
+
+    @Override
+    public void addTooltip(ItemStack stack, TooltipContext context, ITooltip tooltip, TooltipFlag flag)
+    {
+        if (!utils.text.showExtendedDescription(tooltip)) return;
+
+        for (int i = 1; i <= 6; i++)
+        {
+            tooltip.addLine(getItemDescriptionId(), i).apply();
+        }
+    }
+
+    private String getItemDescriptionId()
+    {
+        return "item.pouchandpaper.pouch";
     }
 }
