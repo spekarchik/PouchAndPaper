c3861b943548eb6526fa543ab18db719d98491c9
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-16 18:06:09 +0200

    Now pouches are placed pseudo randomly

---
+++ src/main/java/com/pekar/pouchandpaper/blocks/block_items/PouchOfSeedsBlockItem.java
@@ -0,0 +1,12 @@
+package com.pekar.pouchandpaper.blocks.block_items;
+
+import com.pekar.pouchandpaper.blocks.ModBlockItem;
+import net.minecraft.world.level.block.Block;
+
+public class PouchOfSeedsBlockItem extends ModBlockItem
+{
+    public PouchOfSeedsBlockItem(Block block, Properties properties)
+    {
+        super(block, properties);
+    }
+}
---
+++ src/main/java/com/pekar/pouchandpaper/blocks/block_items/PouchOfWheatSeedsBlockItem.java
@@ -0,0 +1,11 @@
+package com.pekar.pouchandpaper.blocks.block_items;
+
+import net.minecraft.world.level.block.Block;
+
+public class PouchOfWheatSeedsBlockItem extends PouchOfSeedsBlockItem
+{
+    public PouchOfWheatSeedsBlockItem(Block block, Properties properties)
+    {
+        super(block, properties);
+    }
+}
