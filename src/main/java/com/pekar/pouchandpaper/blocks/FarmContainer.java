19bd92361c4ff6785636dc8c93325c37170eef68
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-19 20:51:16 +0200

    Avoiding to place sacks and pouches on non solid blocks
    (cherry picked from commit 5bb1f139a87fd7ab6cbc29359e25404cebf56946)

--- src/main/java/com/pekar/pouchandpaper/blocks/FarmContainer.java
+++ src/main/java/com/pekar/pouchandpaper/blocks/FarmContainer.java
@@ -8,10 +8,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.redstone.Orientation;
 import net.minecraft.world.phys.BlockHitResult;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import org.jetbrains.annotations.Nullable;
@@ -128,8 +131,45 @@ public abstract class FarmContainer extends ModBlock
             {
                 popResourceFromFace(level, pos, player.getDirection().getOpposite(), new ItemStack(getSeedsItem(), seedsInside));
             }
+
+            popResourceFromFace(level, pos, player.getDirection().getOpposite(), getPouchBlock().toStack());
+        }
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos)
+    {
+        var blockBelow = level.getBlockState(pos.below());
+        return blockBelow.isSolidRender()
+                || blockBelow.is(Blocks.FARMLAND)
+                || blockBelow.is(Blocks.DIRT_PATH);
+    }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, @Nullable Orientation orientation, boolean movedByPiston)
+    {
+        if (!canSurvive(state, level, pos))
+        {
+            var blockEntity = level.getBlockEntity(pos);
+            if (!level.isClientSide() && blockEntity instanceof FarmContainerBlockEntity containerBlockEntity)
+            {
+                int seedsInside = containerBlockEntity.getSeedsInside();
+                if (seedsInside > 0)
+                {
+                    popResource(level, pos, new ItemStack(getSeedsItem(), seedsInside));
+                }
+
+                popResource(level, pos, getPouchBlock().toStack());
+                level.removeBlock(pos, false);
+            }
         }
 
-        popResourceFromFace(level, pos, player.getDirection().getOpposite(), getPouchBlock().toStack());
+        super.neighborChanged(state, level, pos, neighborBlock, orientation, movedByPiston);
     }
+
+//    @Override
+//    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
+//    {
+//        return Shapes.empty();
+//    }
 }
