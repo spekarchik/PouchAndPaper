439072a8abb97d0a9edcb2ef0db5231941b92e5d
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-23 20:17:00 +0200

    Pouches: changed logic and states

--- src/main/java/com/pekar/pouchandpaper/blocks/FarmSack.java
+++ src/main/java/com/pekar/pouchandpaper/blocks/FarmSack.java
@@ -1,28 +1,14 @@
 package com.pekar.pouchandpaper.blocks;
 
 import com.google.common.collect.ImmutableMap;
-import com.pekar.pouchandpaper.blocks.entity.FarmContainerBlockEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.ItemInteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
 import java.util.function.Function;
 
 public abstract class FarmSack extends FarmContainer
@@ -60,28 +46,9 @@ public abstract class FarmSack extends FarmContainer
     private static final VoxelShape COLLISION_EMPTY2 = Shapes.create(0.046875 + 0.125, 0.0, 0.0625 + 0.125, 0.8125 - 0.125, 0.5, 0.9375 - 0.125);
     private static final VoxelShape COLLISION_EMPTY3 = Shapes.create(0.09375 + 0.125, 0.0, 0.125 + 0.125, 0.90625 - 0.125, 0.5, 0.9375 - 0.125);
 
-    public static final IntegerProperty FILL_LEVEL = IntegerProperty.create("fill_level", 0, 2);
-
     public FarmSack(Properties properties)
     {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(FILL_LEVEL, 0));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
-    {
-        builder.add(FILL_LEVEL, PLACING_OPTION);
-    }
-
-    @Override
-    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context)
-    {
-        var pos = context.getClickedPos();
-        long seed = mixCoords(pos.getX(), pos.getY(), pos.getZ());
-        var rand = new Random(seed);
-        int placingOption = rand.nextInt(4);
-        return defaultBlockState().setValue(FILL_LEVEL, 0).setValue(PLACING_OPTION, placingOption);
     }
 
     @Override
@@ -151,24 +118,6 @@ public abstract class FarmSack extends FarmContainer
     }
 
     @Override
-    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult)
-    {
-        if (stack.is(getPouchBlock().asItem()) || stack.is(getSeedsItem()))
-        {
-            updateFillLevel(state, level, pos);
-        }
-
-        return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
-    }
-
-    @Override
-    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult)
-    {
-        updateFillLevel(state, level, pos);
-        return super.useWithoutItem(state, level, pos, player, hitResult);
-    }
-
-    @Override
     public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
     {
         int fillLevel = state.getValue(FILL_LEVEL);
@@ -201,28 +150,5 @@ public abstract class FarmSack extends FarmContainer
                 default -> throw new IllegalStateException("Unexpected placingOption: " + placingOption);
             };
         };
-    }
-
-    private void updateFillLevel(BlockState state, Level level, BlockPos pos)
-    {
-        var blockEntity = level.getBlockEntity(pos);
-        if (blockEntity instanceof FarmContainerBlockEntity containerBlockEntity)
-        {
-            int seeds = containerBlockEntity.getSeedsInside();
-            int fillLevel;
-
-            if (seeds > 42)
-                fillLevel = 2;
-            else if (seeds > 21)
-                fillLevel = 1;
-            else
-                fillLevel = 0;
-
-            if (state.getValue(FILL_LEVEL) != fillLevel)
-            {
-                var newState = state.setValue(FILL_LEVEL, fillLevel);
-                level.setBlock(pos, newState, Block.UPDATE_ALL);
-            }
-        }
     }
 }
