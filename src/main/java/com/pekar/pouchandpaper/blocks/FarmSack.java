9bb75bdbe1b39a955180f63c92584c44c4f8a845
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-23 21:49:45 +0200

    Sacks: Translated comments to English
    (cherry picked from commit 05e5ec0f53a5b7051c3706b6a3b526813f09b2a3)
    (cherry picked from commit 7f3c4461fb44e7eef50d61b8bd36f0bb3df1290a)

--- src/main/java/com/pekar/pouchandpaper/blocks/FarmSack.java
+++ src/main/java/com/pekar/pouchandpaper/blocks/FarmSack.java
@@ -28,19 +28,19 @@ public abstract class FarmSack extends FarmContainer
     private static final VoxelShape SHAPE_EMPTY2 = Shapes.create(0.046875, 0.0, 0.0625, 0.8125, 0.5, 0.9375);
     private static final VoxelShape SHAPE_EMPTY3 = Shapes.create(0.09375, 0.0, 0.125, 0.90625, 0.5, 0.9375);
 
-    // FULL – высота 1.0 → 0.99, ширина -2 пикселя с каждой стороны (0.125 = 2px)
+    // FULL – height 1.0 → 0.99, width -2 pixels for each side (0.125 = 2px)
     private static final VoxelShape COLLISION_FULL = Shapes.create(0.1875 + 0.125, 0.0, 0.0625 + 0.125, 0.84375 - 0.125, 0.99, 0.9375 - 0.125);
     private static final VoxelShape COLLISION_FULL1 = Shapes.create(0.0625 + 0.125, 0.0, 0.25 + 0.125, 0.9375 - 0.125, 0.99, 0.90625 - 0.125);
     private static final VoxelShape COLLISION_FULL2 = Shapes.create(0.078125 + 0.125, 0.0, 0.09375 + 0.125, 0.828125 - 0.125, 0.99, 0.953125 - 0.125);
     private static final VoxelShape COLLISION_FULL3 = Shapes.create(0.109375 + 0.125, 0.0, 0.109375 + 0.125, 0.953125 - 0.125, 0.99, 0.953125 - 0.125);
 
-    // SEMI – высота 0.75 → 0.74, ширина -2 пикселя
+    // SEMI – height 0.75 → 0.74, width -2 pixels
     private static final VoxelShape COLLISION_SEMI = Shapes.create(0.1875 + 0.125, 0.0, 0.0625 + 0.125, 0.84375 - 0.125, 0.74, 0.9375 - 0.125);
     private static final VoxelShape COLLISION_SEMI1 = Shapes.create(0.0625 + 0.125, 0.0, 0.25 + 0.125, 0.9375 - 0.125, 0.74, 0.90625 - 0.125);
     private static final VoxelShape COLLISION_SEMI2 = Shapes.create(0.078125 + 0.125, 0.0, 0.09375 + 0.125, 0.828125 - 0.125, 0.74, 0.953125 - 0.125);
     private static final VoxelShape COLLISION_SEMI3 = Shapes.create(0.109375 + 0.125, 0.0, 0.109375 + 0.125, 0.953125 - 0.125, 0.74, 0.953125 - 0.125);
 
-    // EMPTY – высота без изменений, только ширина
+    // EMPTY – height w/o changes, only width updated
     private static final VoxelShape COLLISION_EMPTY = Shapes.create(0.1875 + 0.125, 0.0, 0.0625 + 0.125, 0.84375 - 0.125, 0.5, 0.9375 - 0.125);
     private static final VoxelShape COLLISION_EMPTY1 = Shapes.create(0.0625 + 0.125, 0.0, 0.234375 + 0.125, 0.953125 - 0.125, 0.5, 0.890625 - 0.125);
     private static final VoxelShape COLLISION_EMPTY2 = Shapes.create(0.046875 + 0.125, 0.0, 0.0625 + 0.125, 0.8125 - 0.125, 0.5, 0.9375 - 0.125);
