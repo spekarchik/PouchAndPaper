a41a46fccce0e12eb16d60ddbeb0cba080946848
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-05-24 21:24:43 +0200

    Changed pouch and crop sack breaking behavior: When broken while sneaking (shift), the block now drops full pouches/sacks instead of individual seeds/crops (one pouch per 4 units)

--- src/main/java/com/pekar/pouchandpaper/blocks/block_items/FarmSackBlockItem.java
+++ src/main/java/com/pekar/pouchandpaper/blocks/block_items/FarmSackBlockItem.java
@@ -31,7 +31,7 @@ public class FarmSackBlockItem extends ModBlockItem implements ITooltipProvider
     {
         if (!utils.text.showExtendedDescription(tooltip)) return;
 
-        for (int i = 1; i <= 6; i++)
+        for (int i = 1; i <= 7; i++)
         {
             tooltip.addLine(getItemDescriptionId(), i).apply();
         }
