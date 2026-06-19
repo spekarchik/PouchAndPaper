b3111d80fac9dc17e01629927a60b291d0612b88
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-06-19 20:38:22 +0200

    - Pouches, sacks and packs can now be placed on **Glass** and other full blocks where placement was previously not possible.
    (cherry picked from commit 57d8f10f8838a2db3a21523f047f7abfbf7c97d6)
    (cherry picked from commit cc6a8990bc6ef0f3b5d34ba523d0b596eaeb5df7)

--- src/main/java/com/pekar/pouchandpaper/blocks/FarmContainer.java
+++ src/main/java/com/pekar/pouchandpaper/blocks/FarmContainer.java
@@ -225,7 +225,7 @@ public abstract class FarmContainer extends ModBlock
     protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos)
     {
         var blockBelow = level.getBlockState(pos.below());
-        return blockBelow.isSolidRender()
+        return blockBelow.isCollisionShapeFullBlock(level, pos.below())
                 || blockBelow.is(Blocks.FARMLAND)
                 || blockBelow.is(Blocks.DIRT_PATH);
     }
